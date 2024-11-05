a1f17a501a6bd21f70e7deb23692cb3239b4d2dd
Syncware <walter.pintor@gmx.com>
2024-11-05 14:40:58 +0800

    73. Add Konva dependency and implement basic drawing in App.svelte

--- layers/src/App.svelte
+++ layers/src/App.svelte
@@ -1,3 +1,50 @@
 <script>
-  
+  import { onMount } from "svelte";
+  import Konva from "konva";
+  let container;
+
+  onMount(() => {
+    const stage = new Konva.Stage({
+      container,
+      width: 600,
+      height: 400,
+    });
+    const layer = new Konva.Layer();
+    const rect1 = new Konva.Rect({
+      x: 20,
+      y: 20,
+      width: 100,
+      height: 60,
+      fill: "purple",
+      stroke: "white",
+      strokeWidth: 4,
+    });
+    layer.add(rect1);
+
+    const rect2 = new Konva.Rect({
+      x: 150,
+      y: 20,
+      width: 100,
+      height: 100,
+      fill: "aqua",
+      shadowColor: "rgba(1,1,1,0.5)",
+      shadowBlur: 10,
+      cornerRadius: 20,
+      stroke: "white",
+      strokeWidth: 4,
+    });
+    layer.add(rect2);
+
+    stage.add(layer);
+  });
 </script>
+
+<div bind:this={container} />
+
+
+<!-- <Stage width={300} height={400}>
+  <Layer>
+    <Rect x={20} y={20} width={100} height={60} fill="purple" stroke="white" strokeWidth={4} />
+    <Rect x={150} y={20} width={100} height={100} fill="aqua" shadowColor="rgba(1,1,1,0.5)" shadowBlur={10} cornerRadius={20} stroke="white" strokeWidth={4} />
+  </Layer>
+</Stage> -->
