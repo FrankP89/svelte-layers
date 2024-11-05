3ed3347bc135a461ff3b05926f5cb86e8351a4a8
Syncware <walter.pintor@gmx.com>
2024-11-05 15:27:30 +0800

    75. Creating components to handle Stage, Layer and Shapes as objects

---
+++ layers/src/lib/konva/Layer.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Konva from "konva";
+  import { getContext, setContext } from "svelte";
+  import { stageKey, layerKey } from "./context-keys";
+
+  const { getStage } = getContext(stageKey);
+  const stage = getStage();
+  const layer = new Konva.Layer();
+
+  setContext(layerKey, layer); // Since layer is not undefined at any point, there's no need to create a getLayer function
+
+  stage.add(layer);
+</script>
+
+<slot />
--- layers/src/lib/konva/Stage.svelte
+++ layers/src/lib/konva/Stage.svelte
@@ -1,28 +1,35 @@
 <script>
-    import Konva from 'konva';
-    import { onDestroy, onMount } from 'svelte';
+  import Konva from "konva";
+  import { onDestroy, onMount, setContext } from "svelte";
+  import { stageKey } from "./context-keys";
 
-    // props
-    export let width;
-    export let height;
+  // props
+  export let width;
+  export let height;
 
-    let container;
-    let stage;
+  let container;
+  let stage; // Stage can be undefined if not mounted
 
-    onMount(() => {
-        stage = new Konva.Stage({
-            container: container,
-            width: {width},
-            height: {height}
-        })
+  setContext(stageKey, {
+    getStage: () => stage,
+  });
+
+  onMount(() => {
+    stage = new Konva.Stage({
+      container: container,
+      width: width ,
+      height: height ,
     });
+  });
 
-    onDestroy(() => {
-        if(stage) stage.destroy();
-    });
-
+  onDestroy(() => {
+    if (stage) stage.destroy();
+  });
 </script>
 
 <div bind:this={container}>
-    <slot/>
+    <!-- Only render if stage is defined -->
+    {#if stage} 
+        <slot />
+    {/if}
 </div>
